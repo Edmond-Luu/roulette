f0ddb40e34ff2a70c774cd4080370a028ea51535
Edmond Luu <eluu472@gmail.com>
2022-11-08 16:50:37 -0800

    Fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
-            <p className="caption">Made with ❤️ by <a href="https://edmond-luu.github.io" target="_blank" rel="noreferrer">Edmond Luu</a></p>
+            <p className="caption">Made completely from scratch with ❤️ by <a href="https://edmond-luu.github.io" target="_blank" rel="noreferrer">Edmond Luu</a></p>
         </div>
   )
 }
